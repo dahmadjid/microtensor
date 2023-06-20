6159ddf738d133003df9e5f5f4406659eee34c1e
dahmadjid <dahmadjid@gmail.com>
2023-06-20 18:10:16 +0100

    added dev mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <fmt/format.h>
 #include "files.h"
 #include "microtensor.h"
-
+#include <chrono>
+using std::chrono::high_resolution_clock;
 int main() {
     auto names = io::read_text_file("data/names.txt");
-    auto a = microtensor::Tensor<int>({1, 2, 3, 4, 5, 6} ,{3, 2});
-    auto b = microtensor::Tensor<int>({1, 2, 3 ,4, 5,6, 7, 8, 9 ,10 ,11 ,12}, {2, 6});
-    std::cout << a << '\n';
-    std::cout << b << '\n';
+    std::cout << 6816232977 /1000 /1000 << '\n';
+    auto now = high_resolution_clock::now();
+    auto a = microtensor::Tensor<double>::rand_normal_distrubution({1024, 1024});
+    auto b = microtensor::Tensor<double>::rand_normal_distrubution({1024, 1024});
+    std::cout << high_resolution_clock::now() - now << "\n";
+    // std::cout << a << '\n';
+    // std::cout << b << '\n';
+    auto now2 = high_resolution_clock::now();
     auto c = a * b;
-    std::cout << c << '\n';
+    std::cout << high_resolution_clock::now() - now2 << "\n";
+    // std::cout << c << '\n';
 }       
